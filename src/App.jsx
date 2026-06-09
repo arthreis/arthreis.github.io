b51c0723b4e62e1e12aa832a00965fcb45e7c3ae
Arthur Reis <arr90rj@gmail.com>
2026-06-08 21:36:04 -0300

    chore: migrate from create-react-app to vite and upgrade dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Routes />
+    </Fragment>
+  );
+}
+
+export default App;
